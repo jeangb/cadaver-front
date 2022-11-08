e7cdc8e09caecf4df94b80cdc9c9edf63feb14c8
jeangb <jeangb@hotmail.fr>
2022-11-08 14:46:42 +0100

    [feat] passage de l'apiUrl en https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://cadavreexquis.fr:8080',
+  apiUrl: 'https://cadavreexquis.fr:8080',
   msgErrorIdentifyYourself: 'Essayez de vous identifier de nouveau.'
 };

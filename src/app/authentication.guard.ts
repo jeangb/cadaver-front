55b6561ef3874c189e9622ce450efd7df94636e6
jeangb <jeangb@hotmail.fr>
2025-03-31 12:13:54 +0200

    #9 [feat] updated angular/cli to 16

--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationGuard implements CanActivate, CanActivateChild, CanLoad {
+export class AuthenticationGuard  {
 
   constructor(
     private authService: AuthenticationService,
